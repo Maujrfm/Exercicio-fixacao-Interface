d378c7decca42956e55c445c8fbf8cbc80fd613e
Mauricio Junior <mjrfmoraes@outlook.com>
2022-09-12 21:20:32 -0400

    Exercicio de fixacao sobre interface pronto

---
+++ Exercicio Fixacao Interface/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using Exercicio_Fixacao_Interface.Entities;
+using Exercicio_Fixacao_Interface.Services;
+
+namespace Exercicio_Fixacao_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data ");
+            Console.Write("Number: ");
+            int contracNumber = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract myContract = new Contract(contracNumber, contractDate, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+            Console.WriteLine("Installments: ");
+            foreach(Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Exercicio Fixacao Interface/Services/IOnlinePaymentService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Exercicio_Fixacao_Interface.Services
+{
+    interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
